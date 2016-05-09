9300ff70c36a302a1025cacd007ef499deff1be5
Valentin Baryshev <valentin.barishev@phystech.edu>
2016-05-09 16:17:41 +0400

    fixed project structure

--- MiniJavaCompiler/LifeAnalysis/LiveAnalysis.cpp
+++ MiniJavaCompiler/LifeAnalysis/LiveAnalysis.cpp
@@ -1,14 +1,8 @@
-// Автор: Федюнин Валерий
-// Набор классов для анализа времени жизни переменных
-
 #include "LiveAnalysis.h"
-
 #include <assert.h>
 
 
 namespace Assembler {
-
-	// =====================================================================================================================
 
 	CGraph::CGraph(int size) : nodes(size) {}
 
@@ -34,24 +28,18 @@ namespace Assembler {
 		return nodes[index];
 	}
 
-	// =====================================================================================================================
-
 	CWorkFlowGraph::CWorkFlowGraph(const std::list<const CBaseInstruction*>& asmFunction) : CGraph(asmFunction.size())
 	{
 		buildLabelMap(asmFunction);
 		addEdges(asmFunction);
 	}
 
-
-	// составляет соответствие между метками и вершинами графа
 	void CWorkFlowGraph::buildLabelMap(const std::list<const CBaseInstruction*>& asmFunction)
 	{
 		int nodeIndex = 0;
 		for (auto cmd : asmFunction) {
-			// Проверка, ялвяется ли интрукция меткой
 			const CLabel* label = dynamic_cast<const CLabel*>(cmd);
 			if (label != nullptr) {
-				// если инструкция является меткой то добавим ее в соответствие
 				assert(label->JumpTargets() != nullptr);
 				assert(label->JumpTargets()->Head() != nullptr);
 				labels.insert(std::make_pair(label->JumpTargets()->Head()->GetName()->GetString, nodeIndex));
@@ -61,7 +49,6 @@ namespace Assembler {
 	}
 
 
-	// добавляет ребра в граф
 	void CWorkFlowGraph::addEdges(const std::list<const CBaseInstruction*>& asmFunction)
 	{
 		int nodeIndex = 0;
@@ -77,14 +64,10 @@ namespace Assembler {
 		}
 	}
 
-	// =====================================================================================================================
 
 	namespace {
-		// не вижу смысла объявлять это в хедере т.к. это сугубо вспомогательная конструкция
 		class CTopSort {
 		public:
-			// Топологическая сортировка графа, начиная с вершины start
-			// притом недостижимые вершины игнорируются
 			static std::vector<int> topSort(const CGraph& graph, int start)
 			{
 				res.clear();
@@ -98,12 +81,9 @@ namespace Assembler {
 			}
 
 		private:
-			// вектор в котором будет хранится результат
 			static std::vector<int> res;
-			// вектор с пометками, была ли посещена обходом данная вершина
 			static std::vector<char> marked;
 
-			// обход в глубину с запоминанием порядка выхода (для топологической соритровки)
 			static void dfs(const CGraph& graph, int nodeIndex)
 			{
 				marked[nodeIndex] = true;
@@ -121,15 +101,13 @@ namespace Assembler {
 
 		std::vector<int> CTopSort::res = std::vector<int>();
 		std::vector<char> CTopSort::marked = std::vector<char>();
-	} // anonymous namespace
-
-	  // =====================================================================================================================
+	} 
 
 	CLiveInOutCalculator::CLiveInOutCalculator(const std::list<const CBaseInstruction*>& asmFunction) :
 		workflow(asmFunction), liveIn(workflow.Size()), liveOut(workflow.Size())
 	{
 		bool setsChanged = true;
-		int mainFuncIndex = 0; // TODO: нормально найти начальную вершину
+		int mainFuncIndex = 0; 
 		std::vector<int> revTopsort = CTopSort::topSort(workflow, mainFuncIndex);
 		buildCommands(asmFunction);
 		buildDefines(asmFunction);
@@ -140,8 +118,6 @@ namespace Assembler {
 			setsChanged = false;
 			for (auto nodeIndex : revTopsort) {
 				std::set<std::string> newLiveIn = liveOut[nodeIndex];
-
-				// out[n] - def[n]
 				auto currList = commands[nodeIndex]->DefindedVars();
 				while (currList != nullptr  &&  currList->Head() != nullptr) {
 					auto inSet = newLiveIn.find(currList->Head()->GetName()->GetString());
@@ -150,24 +126,17 @@ namespace Assembler {
 					}
 					currList = currList->Tail();
 				}
-
-				// in[n] = use[n] V ( out[n] - def[n] )
 				currList = commands[nodeIndex]->UsedVars();
 				while (currList != nullptr  &&  currList->Head() != nullptr) {
 					newLiveIn.insert(currList->Head()->GetName()->GetString());
 					currList = currList->Tail();
 				}
-
-				// out[n] = V in[s]		where s in succ[n]
 				std::set<std::string> newLiveOut;
 				for (auto succ : workflow.GetNode(nodeIndex).out) {
 					for (auto var : liveIn[succ]) {
 						newLiveOut.insert(var);
 					}
 				}
-
-				// если хоть у одной вершины изменилось множество live-in или live-out 
-				// то необходимо провести еще итерацию алгоритма
 				if (!theSame(newLiveIn, liveIn[nodeIndex]) || !theSame(newLiveOut, liveOut[nodeIndex])) {
 					setsChanged = true;
 					liveIn[nodeIndex] = newLiveIn;
@@ -268,4 +237,4 @@ namespace Assembler {
 		}
 	}
 
-} // namespace Assembler
+} 
--- MiniJavaCompiler/LifeAnalysis/LiveAnalysis.h
+++ MiniJavaCompiler/LifeAnalysis/LiveAnalysis.h
@@ -9,45 +9,28 @@
 
 
 namespace Assembler {
-	// Вершина графа
 	struct CNode {
-		// входящие ребра
 		std::vector<int> in;
-		// исходящие ребра
 		std::vector<int> out;
 	};
 
-
-	// Граф
 	class CGraph {
 	public:
 		explicit CGraph(int size);
-
-		// добавить ребро в граф из вершины from в вершину to
 		void AddEdge(int from, int to);
-
-		// Количество вершин в графе
 		int Size() const;
-
-		// получить вершину
 		const CNode& GetNode(int index) const;
 
 	protected:
-		// узлы графа
-		// вся информация о ребрах хранится внутри вершин
 		std::vector<CNode> nodes;
 	};
 
 
-	// Граф потока управления
 	class CWorkFlowGraph : public CGraph {
 	public:
-		// Конструктор
-		// принимает на вход map где ключ - имя функции а значение - список ассемблерных команд
 		explicit CWorkFlowGraph(const std::list<const CBaseInstruction*>& asmFunction);
 
 	private:
-		// соответствие между метками и вершинами графа
 		std::map<std::string, int> labels;
 
 		void buildLabelMap(const std::list<const CBaseInstruction*>& asmFunction);
@@ -55,37 +38,22 @@ namespace Assembler {
 	};
 
 
-	// Граф подсчета live-in live-out (за подробностями см "Modern Compiler Implementation in Java 4 ed." глава 10
 	class CLiveInOutCalculator {
 	public:
-		// Конструктор
-		// принимает на вход map где ключ - имя функции а значение - список ассемблерных команд
 		explicit CLiveInOutCalculator(const std::list<const CBaseInstruction*>& asmFunction);
-
-		// получить список live-in переменных для некоторой вершины/инструкции
 		const std::set<std::string>& GetLiveIn(int nodeIndex) const;
-
-		// получить список live-out переменных для некоторой вершины/инструкции
 		const std::set<std::string>& GetLiveOut(int nodeIndex) const;
-
-		// получить список объявляемых переменных для некоторой вершины/инструкции
 		const std::set<std::string>& GetDefines(int nodeIndex) const;
-
 		const std::set<std::string>& GetUses(int nodeIndex) const;
 
 	private:
-		// граф потока управления
 		CWorkFlowGraph workflow;
 
-		// множества live-in и live-out переменных для каждой из вершин графа
 		std::vector<std::set<std::string>> liveIn;
 		std::vector<std::set<std::string>> liveOut;
-
-		// множества defines и uses
 		std::vector<std::set<std::string>> defines;
 		std::vector<std::set<std::string>> uses;
 
-		// вектор с ассемблерными командами
 		std::vector<const CBaseInstruction*> commands;
 
 		bool theSame(const std::set<std::string>& x, const std::set<std::string>& y) const;
@@ -93,4 +61,4 @@ namespace Assembler {
 		void buildDefines(const std::list<const CBaseInstruction*>& asmFunction);
 		void buildUses(const std::list<const CBaseInstruction*>& asmFunction);
 	};
-} // namespace Assembler
+}
